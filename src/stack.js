9855f95edc507c582945301bc962ad4140e7c2db
Margarita Dobrenco <solarsungai@gmail.com>
2025-11-07 11:24:37 +0200

    feat: stack done

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,20 @@ const { NotImplementedError } = require('../lib/errors');
  *
  */
 class Stack {
-  push(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor() {
+    this.storage = [];
+  }
+
+  push(value) {
+    this.storage.push(value);
   }
 
   pop() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.storage.pop();
   }
 
   peek() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+   return this.storage[this.storage.length - 1];
   }
 }
 
